6fe63d7f1156ea4aceabd18be07830c14cda91ec
João Victor de Lima Muniz <joao.muniz@ciandt.com>
2024-04-16 06:50:40 -0300

    Passing arguments to spark submit

--- dags/data_api_process.py
+++ dags/data_api_process.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import timedelta
 from airflow.utils.dates import days_ago
 
@@ -39,11 +40,19 @@ get_api_task = SimpleHttpOperator(
 )
 
 medallion_stage_task = SparkSubmitOperator(
-    task_id='medallion_stage',
-    application='/opt/airflow/dags/spark_script.py',
-    conn_id='spark_local',
-    dag=dag
-)
+        task_id='medallion_stage',
+        application='/opt/airflow/dags/spark_script.py',
+        conn_id='spark_local',
+        application_args=["{{ task_instance.xcom_pull(task_ids='api_request') }}"],
+        packages="org.apache.spark:spark-avro_2.12:3.4.2",
+        total_executor_cores=2,
+        executor_cores=2,
+        executor_memory='1G',
+        driver_memory='1G',
+        execution_timeout=timedelta(minutes=10),
+        dag=dag
+    )
+
 
 end_task = DummyOperator(
     task_id='end_task',
